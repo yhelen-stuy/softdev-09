2ba4d64450bc32ba38210d2fa2d648feeb1c1470
Helen Ye <00yhelen@gmail.com>
2017-10-14 23:06:15 -0400

    Fix db_builder

--- db_builder.py
+++ db_builder.py
@@ -16,23 +16,26 @@ c = db.cursor()    #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
-def read_csv(filename):
-    return csv.DictReader(filename)
-
-courses = read_csv('courses.csv')
-peeps = read_csv('peeps.csv')
-
 def create_table(table_name, columns):
     return "CREATE TABLE " + table_name + " (" + columns + ")"
 
-def insert_data(csv_dict, table_name):
-    return
+def insert_data(csvfile, table_name):
+    f = open(csvfile, 'rU')
+    reader = csv.DictReader(f)
+    for row in reader:
+        comm = "INSERT INTO " + table_name + ' VALUES ('
+        for field in row:
+            comm += '"' + row[field] + '",'
+        comm = comm[0:-1]
+        comm += ")"
+        c.execute(comm)
+    f.close()
 
 c.execute(create_table('courses', 'code TEXT, mark INTEGER, id INTEGER'))
 c.execute(create_table('peeps', 'name TEXT, age INTEGER, id INTEGER'))
+insert_data('courses.csv', 'courses')
+insert_data('peeps.csv', 'peeps')
 
 #==========================================================
 db.commit() #save changes
 db.close()  #close database
-
-
